e1b9e0a6ef2be9fbfd8f2969cd4b0ae07c1a6c2b
Chosenundead15 <snaider1507@tutanota.com>
2021-03-13 10:50:25 -0400

    some basic data for ShipBase, still does nothing (#1)

--- scenes/RoomBase.gd
+++ scenes/RoomBase.gd
@@ -3,7 +3,7 @@ extends Node2D
 onready var _room_outline = $"RoomOutline-maybe-shader"
 
 func _show_mouse_outline() -> void:
-    _room_outline.show()
-    
+	_room_outline.show()
+	
 func _hide_mouse_outline() -> void:
-    _room_outline.hide()
+	_room_outline.hide()
