28b9d4c1d336a14e21c80508f5c18d3a377ee025
Chosenundead15 <snaider1507@tutanota.com>
2021-03-13 10:39:48 -0400

    some basic data for ShipBase, still does nothing

--- scenes/ShipBase.gd
+++ scenes/ShipBase.gd
@@ -3,6 +3,14 @@ extends Node2D
 # Put data here for the overall ship.
 #  Export it mostly and then we can have different values per ship.
 
+export(int) var commsRange = 0 
+export(int) var food = 0
+export(int) var fuel = 0
+export(int) var medicines = 0
+export(int) var integrity = 0
+export(int) var shieldCap = 0
+var topSpeed = 0 #may need to be passed to the engines
+
 # Each of the rooms will signal up to the ShipBase when changes
 #  in status have to occur based on the room.
 
