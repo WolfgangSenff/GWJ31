45d1a95d767fd8136e926c13a522b3c3ceb51d93
Kyle Szklenski <kyle.szklenski@gmail.com>
2021-03-13 17:43:46 -0500

    Initial ship, room implementation (#2)

--- scenes/RoomBase.gd
+++ scenes/RoomBase.gd
@@ -1,9 +1,38 @@
 extends Node2D
 
+signal room_activated(room, command, data)
+
 onready var _room_outline = $"RoomOutline-maybe-shader"
+onready var _activation_popup = $CanvasLayer/ActivationPopup
+# Notice the above is not exclusive so the player can tap
+#  outside the view to close it, in which case nothing should
+#  happen
+onready var content_container = $CanvasLayer/ActivationPopup/MainContainer/ContentContainer
+
+export (String) var RoomId # Use this to target specific rooms with specific obstacles
 
 func _show_mouse_outline() -> void:
-	_room_outline.show()
-	
+    _room_outline.show()
+    
 func _hide_mouse_outline() -> void:
-	_room_outline.hide()
+    _room_outline.hide()
+
+func _room_input(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+    if event is InputEventMouseButton and event.is_pressed():
+        _handle_mouse_input()
+
+# When the room is extended, override this to handle what happens.
+#  The default for now is to show the ActivationPopup
+func _handle_mouse_input():
+    _activation_popup.popup_centered_ratio(.8)
+    # Chose .8 for no real reason
+    
+
+# Emit room_activated when this room actually executes its command,
+#  so like the Helm would emit the signal when the player changes
+#  the direction the ship is going
+
+func room_activated(command, data) -> void:
+    emit_signal("room_activated", self, command, data)
+    # All this does is emits the signal; however, individual
+    #  rooms should call this when needed
---
+++ scenes/gui/CircularTimedProgress.gd
@@ -0,0 +1,33 @@
+extends TextureProgress
+
+signal is_refreshed
+
+export (int) onready var TimeToRefresh setget set_refresh_time
+
+onready var timer = $Timer
+
+var is_ready = true
+
+func _ready() -> void:
+    timer.connect("timeout", self, "_on_timer_timeout")
+    set_refresh_time(TimeToRefresh)
+    
+# Have a separate function in case we want a room which can
+#  reduce these timers globally or something.
+func set_refresh_time(time) -> void:
+    max_value = time
+    TimeToRefresh = time
+    
+func begin_refresh() -> void:
+    is_ready = false
+    value = max_value
+    show()
+    timer.start()
+    
+func _on_timer_timeout() -> void:
+    value -= 1
+    if value <= 0:
+        is_ready = true
+        timer.stop()
+        hide()
+        emit_signal("is_refreshed")
