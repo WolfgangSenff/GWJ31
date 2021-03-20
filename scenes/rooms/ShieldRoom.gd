1d77c80b2191451e6e8681485dddf96dcc392a96
Kyle Szklenski <kyle.szklenski@gmail.com>
2021-03-20 11:37:36 -0400

    Starbases (#18)
    * Starbase landing pads
    
    * Initial starbase commit
    
    * Starbases 2
    
    * Big starbase update

--- scenes/rooms/ShieldRoom.gd
+++ scenes/rooms/ShieldRoom.gd
@@ -7,21 +7,22 @@ var power = true
 onready var button = $CanvasLayer/ActivationPopup/MainContainer/ContentContainer/CenterContainer/Button
 
 func _ready():
-	$Shield.position = shield_pos
+    $Shield.position = shield_pos
 
 func get_max_capacity() -> int:
-	return get_parent().shieldCap
+    return get_parent().shieldCap
 
 
 func _on_Button_pressed():
-	if power == true:
-		button.text = "Turn On"
-		power = false
-		for i in $Shield.get_children():
-			i.get_node("Area2D").lit = power
-	else:
-		button.text = "Turn Off"
-		power = true
-		for i in $Shield.get_children():
-			i.get_node("Area2D").lit = power
+    if power == true:
+        button.text = "Turn On"
+        power = false
+    else:
+        button.text = "Turn Off"
+        power = true
+        
+    var shields = get_tree().get_nodes_in_group("shield_arc")
+    for shield in shields:
+        var area = shield.get_node("Area2D")
+        area.lit = power
 
