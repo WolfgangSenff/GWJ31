bdd5f13c582d76f941b60c1a501136cdf6df242f
Armando <snaider1507@tutanota.com>
2021-03-20 22:43:35 -0400

    fixed some bugs caused by the incorporation of the ship selector

--- scenes/gui/CharacterSelectButton.gd
+++ scenes/gui/CharacterSelectButton.gd
@@ -3,5 +3,5 @@ extends Button
 var character setget set_character
 
 func set_character(value) -> void:
-    character = value
-    text = character.CharacterName
+	character = value
+	text = character.CharacterName
