650e33f17973581e762135918e2cc6d37f49632f
Chosenundead15 <snaider1507@tutanota.com>
2021-03-21 12:19:52 -0400

    all the ships have been implemented, plus minor fix in the selector

--- scenes/weapons/MissileWeapon.gd
+++ scenes/weapons/MissileWeapon.gd
@@ -14,31 +14,31 @@ onready var _sprite = $Sprite
 onready var _trail = $Trail
 
 func _ready() -> void:
-    set_physics_process(false)
-    connect("area_entered", self, "_on_area_entered", [], CONNECT_ONESHOT)
-    
+	set_physics_process(false)
+	connect("area_entered", self, "_on_area_entered", [], CONNECT_ONESHOT)
+	
 
 func _physics_process(delta : float) -> void:
-    _current_lifetime += delta
-    if _current_lifetime > LifeTime:
-        explode()
-        set_physics_process(false)
-    
-    _current_speed += Acceleration * delta
-    _current_speed = clamp(_current_speed, 0, TopSpeed)
-    global_position += _current_speed * Vector2.RIGHT.rotated(global_rotation - deg2rad(90))
+	_current_lifetime += delta
+	if _current_lifetime > LifeTime:
+		explode()
+		set_physics_process(false)
+	
+	_current_speed += Acceleration * delta
+	_current_speed = clamp(_current_speed, 0, TopSpeed)
+	global_position += _current_speed * Vector2.RIGHT.rotated(global_rotation - deg2rad(90))
 
 func _on_area_entered(area) -> void:
-    if area.is_in_group("weapon"):
-        set_physics_process(false)
-        explode()
+	if area.is_in_group("weapon"):
+		set_physics_process(false)
+		explode()
 
 func explode() -> void:
-    _sprite.hide()
-    _trail.hide()
-    _explosion_particles.emitting = true
-    yield(get_tree().create_timer(5), "timeout")
-    queue_free()
+	_sprite.hide()
+	_trail.hide()
+	_explosion_particles.emitting = true
+	yield(get_tree().create_timer(5), "timeout")
+	queue_free()
 
 func _on_Timer_timeout():
-    armed = true
+	armed = true
