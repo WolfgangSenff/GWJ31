8c9df9ca8ec79e550163da0cb04a37a42798d8ed
Kyle Szklenski <kyle.szklenski@gmail.com>
2021-03-21 13:52:51 -0400

    Squash

--- Globals.gd
+++ Globals.gd
@@ -9,9 +9,9 @@ var selectedShip: String
 const SpiceJarScene = preload("res://scenes/collectables/SpiceJar.tscn")
 
 func CameraZoomOut(for_time) -> void:
-	var tree = get_tree()
-	var camera = tree.get_nodes_in_group("Camera")[0]
-	camera.zoom_out(for_time)
+    var tree = get_tree()
+    var camera = tree.get_nodes_in_group("Camera")[0]
+    camera.zoom_out(for_time)
 
 func SpawnOnMain(toSpawn : PackedScene, pos : Vector2, rot : float, shouldEnablePhysics : bool = false):
     var scene = toSpawn.instance()
