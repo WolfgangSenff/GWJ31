cfbedf8952005f7276c9602126a494692f55a4e7
Armando Gonzalez <snaider1507@tutanota.com>
2021-03-21 07:26:31 -0400

    Gui update (#22)
    * added ship selector and fixed some general bugs
    
    * fixed some bugs caused by the incorporation of the ship selector
    
    * added ship selector and fixed some general bugs
    
    * fixed some bugs caused by the incorporation of the ship selector

--- scenes/rooms/ShieldRoom.gd
+++ scenes/rooms/ShieldRoom.gd
@@ -4,25 +4,26 @@ export(Vector2) var shield_pos
 export(int) var shieldCap = 0
 var power = true
 
+
 onready var button = $CanvasLayer/ActivationPopup/MainContainer/ContentContainer/CenterContainer/Button
 
 func _ready():
-    $Shield.position = shield_pos
+	$Shield.position = shield_pos
 
 func get_max_capacity() -> int:
-    return get_parent().shieldCap
+	return get_parent().shieldCap
 
 
 func _on_Button_pressed():
-    if power == true:
-        button.text = "Turn On"
-        power = false
-    else:
-        button.text = "Turn Off"
-        power = true
-        
-    var shields = get_tree().get_nodes_in_group("shield_arc")
-    for shield in shields:
-        var area = shield.get_node("Area2D")
-        area.lit = power
+	if power == true:
+		button.text = "Turn On"
+		power = false
+	else:
+		button.text = "Turn Off"
+		power = true
+		
+	var shields = get_tree().get_nodes_in_group("shield_arc")
+	for shield in shields:
+		var area = shield.get_node("Area2D")
+		area.lit = power
 
